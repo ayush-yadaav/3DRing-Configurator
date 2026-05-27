6ed957d4caf82930234bd6dc211563462a638a16
ayush-yadaav <yadavayush2091a@gmail.com>
2026-05-27 12:37:45 +0530

    error

--- src/components/HeroRingViewer.jsx
+++ src/components/HeroRingViewer.jsx
@@ -7,7 +7,7 @@ function RingModel() {
   const meshRef = useRef();
   
 
-  const { scene } = useGLTF("/models/ring.glb"); 
+  const { scene } = useGLTF("/public/models/ring.glb"); 
 
  
   useFrame((state, delta) => {
